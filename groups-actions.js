1111e25e5683a616e7000f801743a03919563cfa
Edward Seufert <edwardseufert@Edwards-MBP.home>
2020-05-04 08:22:09 -0400

    update preferences form and save

--- groups-actions.js
+++ groups-actions.js
@@ -13,8 +13,8 @@ export function initGroup() {
     let requestParams = {};
     requestParams.action = "INIT";
     requestParams.service = "GROUP_SVC";
-    //requestParams.prefForms = new Array("LOGIN_FORM","REGISTRATION_FORM","FORGOTPASSWORD_FORM","PASSWORD_CHANGE_FORM");
-    requestParams.prefTexts = new Array("GROUP_PAGE");
+    //requestParams.prefFormKeys = new Array("LOGIN_FORM","REGISTRATION_FORM","FORGOTPASSWORD_FORM","PASSWORD_CHANGE_FORM");
+    requestParams.prefTextKeys = new Array("GROUP_PAGE");
     let params = {};
     params.requestParams = requestParams;
     params.URI = '/api/member/callService';
